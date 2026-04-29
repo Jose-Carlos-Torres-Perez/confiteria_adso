3b9283eceaf40ea762a238cd6be4db8251f502b6
Jose carlos torres perez <puntodetalle2019@gmail.com>
2026-04-29 16:51:34 -0500

    se implementa index.php

--- index.php
+++ index.php
@@ -12,17 +12,16 @@
 <body id="index">
     
     <header id="encabezado">
-       
      <?php require_once __DIR__ . "/../confiteria_adso/vistas/header.php" ?>
     </header>
+
     <section id="general">
         <button class="general">Confiteria</button>
         <button class="general">Regalos</button>
     </section>
-    <section id="slider"></section>
     
+    <section id="slider"></section>
    <?php require_once __DIR__ . "/../confiteria_adso/vistas/productos.php" ?>
-
     <section id="banners">
 
 <div class="banner" id="mis-pedidos"></div>
